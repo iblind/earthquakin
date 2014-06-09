1901299683ff9d44fcf490457d49abda64178bba
IB <ib2342@columbia.edu>
2014-06-09 13:02:50 -0400

    Learned about functions

---
+++ Earthquake.py
@@ -0,0 +1,14 @@
+def greeting():
+	name = raw_input("What is your name?")
+	print "Hello world, " + name
+
+def hello(name):
+	print "Hello, " + name
+	
+def add(a, b):	
+	return a + b 
+	
+hello("Buddy")
+
+print add (2, 3)
+
